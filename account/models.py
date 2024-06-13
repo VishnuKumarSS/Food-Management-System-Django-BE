ff7913515e54f365ad2f6bfb48e4a866b9105b64
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-06-14 02:23:22 +0530

    feat: Models additions

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
---
+++ food/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_available = models.IntegerField()
+    quantity_sold = models.IntegerField(default=0)
