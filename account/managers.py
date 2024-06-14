0928b4001283d6e8ffb38b6e9b379f00cd58caaa
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-06-15 00:47:27 +0530

    User and UserManager addition

---
+++ account/managers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class UserAccountManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifier
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, username, password=None, **extra_fields):
+        """
+        Create and save a user with the given email, username, and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        if not username:
+            raise ValueError(_("The Username must be set"))
+        if not password:
+            raise ValueError(_("The Password must be set"))
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        """
+        Create and save a SuperUser with the given email, username, and password.
+        """
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        if extra_fields.get("is_admin") is not True:
+            raise ValueError(_("Superuser must have is_admin=True."))
+
+        return self.create_user(email, username, password, **extra_fields)
--- account/models.py
+++ account/models.py
@@ -1,11 +1,32 @@
 import uuid
 
-from django.contrib.auth.models import AbstractUser
-# from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
+from .managers import UserAccountManager
 
-class User(AbstractUser):
-    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(blank=True, max_length=254, verbose_name='email address', unique=True)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    USERNAME_FIELD = 'email'  # Unique identifier for login
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserAccountManager()
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return self.email
+    
+    def get_user_name(self):
+        return f"{self.first_name} {self.last_name}"
