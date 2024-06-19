e48d3478f037ba747fa437cf88f299607aa5de9b
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-06-19 15:26:48 +0530

    To restrict the normal users from adding food items

--- food/views.py
+++ food/views.py
@@ -8,6 +8,12 @@ class FoodItemListCreateView(generics.ListCreateAPIView):
     queryset = FoodItem.objects.all()
     serializer_class = FoodItemSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [permissions.IsAdminUser]
+        
+        return super().get_permissions()
 
 
 class FoodItemDetailView(generics.RetrieveUpdateDestroyAPIView):
